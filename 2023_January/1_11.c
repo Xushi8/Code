b1607d217768beec1e8332e36dc497105b9ac494
uuufa <trump@niubi.com>
2023-01-11 22:51:46 +0800

    1.11

---
+++ 2023_January/1_11.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+
+// int arr[10001] = { 6,2,5,5,4,5,6,3,7,6 };
+// int judge[10001] = { 0 };
+// int main()
+// {
+//     int i, j, k;
+//     int n;
+//     scanf("%d", &n);
+//     int count = 0;
+//     for (i = 10; i < 10000; i++)
+//     {
+//         j = i;
+//         while (j)
+//         {
+//             arr[i] += arr[j % 10];
+//             j /= 10;
+//         }
+//     }
+
+//     for (i = 0; i < 2000; i++)
+//     {
+//         for (j = 0; j < 2000; j++)
+//         {
+//             if (arr[i] + arr[j] + arr[i + j] == n - 4)
+//             {
+//                 count++;
+//                 if (i == j)
+//                 {
+//                     if (judge[i] == 0)
+//                     {
+//                         judge[i]++;
+//                     }
+//                     else
+//                     {
+//                         count--;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+
+//     printf("%d", count);
+
+//     return 0;
+// }
+
+int main()
+{
+    
+
+    return 0;
+}
