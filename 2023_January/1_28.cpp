878be61663cf75b51b046525bc57f072c7e6d281
uuufa <trump@niubi.com>
2023-01-28 11:33:09 +0800

    test of pr

---
+++ 2023_January/1_28.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    unsigned int a = 0xffffffff;
+    cout << a << endl;
+    cout << a + 1 << endl;
+    cout << a + 2 << endl;
+
+    return 0;
+}
