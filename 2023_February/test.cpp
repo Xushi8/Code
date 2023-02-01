251e64158e37c2e8db263b4f5f5764b62f93ffee
uuufa <trump@niubi.com>
2023-02-01 20:57:44 +0800

    test文件

--- 2023_February/test.cpp
+++ 2023_February/test.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 #include<string>
 #include<algorithm>
+#include<vector>
 using namespace std;
 
 int main()
 {
     
 
+    system("pause");
     return 0;
 }
--- 2023_February/test1.cpp
+++ 2023_February/test1.cpp
@@ -5,6 +5,7 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-
+    
+    system("pause");
     return 0;
 }
