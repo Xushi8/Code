05620b0549cf10e64927ded7ccd3924cdf5f0593
uuufa <trump@niubi.com>
2023-03-03 15:01:05 +0800

    3.3

---
+++ 2023_March/test.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void test1(const vector<int> v)
+{
+    for (auto i = v.begin(); i != v.end(); i++)
+    {
+        cout << *i << '\n';
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    vector<int> V;
+    for (int i = 0; i < 1000000; i++)
+    {
+        
+    }
+
+    system("pause");
+    return 0;
+}
