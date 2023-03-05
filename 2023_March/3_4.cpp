e54e2cb6dd37b84926f2998be7af68af73d2e430
uuufa <trump@niubi.com>
2023-03-05 16:58:49 +0800

    3.4

---
+++ 2023_March/3_4.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const long double PI = 3.1415926535897932384626433832795L;
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    long double a = sin(PI);
+    cout << a << endl;
+
+    cout.put('/');
+    cout.put('/');
+    cout << flush;
+
+    system("pause");
+    return 0;
+}
