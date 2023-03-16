d81a51997929bacd7868b0f0b149c2b56408e494
uuufa <trump@niubi.com>
2023-03-16 23:02:10 +0800

    3.16

---
+++ 2023_March/3_16.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    // int n;
+    // cin >> n;
+    // vector<int> arr(n);
+    // arr.emplace_back(34);
+    // arr.emplace_back(34);
+    // arr.emplace_back(34);
+    // cout << arr.size();
+    // char str[5];
+    // cin >> str;
+    // cout << str;
+
+    // priority_queue<int, vector<int>, greater<int> >q;
+    // q.push(8);
+    // q.push(7);
+    // q.push(9);
+    // q.push(2);
+    // cout << q.top() << '\n';
+    // q.pop();    cout << q.top() << '\n';
+    // q.pop();    cout << q.top() << '\n';
+    // q.pop();    cout << q.top() << '\n';
+    // q.pop();
+
+    // int ch;
+    // while ((ch = cin.get()) != EOF)
+    // {
+    //     if (isspace(ch))
+    //     {
+    //         ch++;
+    //         continue;
+    //     }
+    // }
+
+
+    uint64_t a = 100618342136696320;
+
+
+    cout << flush;
+    system("pause");
+    return 0;
+}
