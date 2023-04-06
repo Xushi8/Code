50b2aa7acc84f5966023115b761f5eeebf41d797
uuufa <trump@niubi.com>
2023-04-06 10:31:04 +0800

    4.5

--- 2023_April/4_4.cpp
+++ 2023_April/4_4.cpp
@@ -145,9 +145,9 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0);
     
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++)
+    int n, e;
+    cin >> n >> e;
+    for (int i = 0; i < e; i++)
     {
         int a, b, c;
         cin >> a >> b >> c;
--- 2023_April/4_5.cpp
+++ 2023_April/4_5.cpp
@@ -47,7 +47,7 @@ using namespace std;
 //     scanf("%d%d", &n, &m);
 //     int ch;
 //     while ((ch = getchar()) != '\n') continue;
-    
+
 //     int cnt = 0, res = 0;
 //     for (int i = 1; i <= n; i++) {
 //         fgets(a[i], 510, stdin);
@@ -61,7 +61,7 @@ using namespace std;
 
 //     if (res == n && cnt < m) puts("Wo AK le");
 //     else printf("%s\n", a[res + 1]);
-    
+
 //     return 0;
 // }
 
@@ -81,7 +81,7 @@ using namespace std;
 //         x *= 10;
 //         x += ch - '0';
 //     }
-    
+
 //     while ((ch = cin.get()) >= '0' && ch <= '9')
 //     {
 //         x *= 10;
@@ -94,7 +94,7 @@ using namespace std;
 // {
 //     ios::sync_with_stdio(false);
 //     cin.tie(0);
-    
+
 //     int n;
 //     cin >> n;
 //     while (n--)
@@ -107,91 +107,137 @@ using namespace std;
 //     return 0;
 // }
 
+// class edge
+// {
+// public:
+//     int from, to, cost;
+//     edge()
+//     {
+
+//     }
+
+//     edge(const int& a, const int& b, const int& c)
+//     {
+//         from = a;
+//         to = b;
+//         cost = c;
+//     }
+// };
+// const int max_v = 1005, max_e = 10005, inf = 0x3f3f3f3f;
+// vector<edge> G(max_e);
+// int dis[max_v];
+// int V, E;
+
+// void bellman_ford(int s)
+// {
+//     for (int i = 0; i < V; i++) dis[i] = inf;
+//     dis[s] = 0;
+//     while (1)
+//     {
+//         bool update = 0;
+//         for (int i = 0; i < E; i++)
+//         {
+//             edge& e = G[i];
+//             if (dis[e.from] != inf && dis[e.to] > dis[e.from] + e.cost)
+//             {
+//                 dis[e.to] = dis[e.from] + e.cost;
+//                 update = 1;
+//             }
+//         }
+//         if (!update) break;
+//     }
+// }
+
+// bool find_negative_loop()
+// {
+//     for (int i = 0; i < V; i++) dis[i] = 0;
+
+//     for (int i = 0; i < V; i++)
+//     {
+//         for (int j = 0; j < E; j++)
+//         {
+//             edge& e = G[j];
+//             if (dis[e.to] > dis[e.from] + e.cost)
+//             {
+//                 dis[e.to] = dis[e.from] + e.cost;
+//                 if (i == V - 1) return true;
+//             }
+//         }
+//     }
+
+//     return false;
+// }
+
+// int main()
+// {
+//     ios::sync_with_stdio(false);
+//     cin.tie(0);
+
+//     cin >> V >> E;
+//     for (int i = 0; i < E; i++)
+//     {
+//         int a, b, c;
+//         cin >> a >> b >> c;
+//         G[i].from = a;
+//         G[i].to = b;
+//         G[i].cost = c;
+//     }
+//     if (find_negative_loop())
+//         cout << "yes\n";
+//     else
+//         cout << "no\n";
+
+//     bellman_ford(0);
+//     for (int i = 0; i < V; i++)
+//     {
+//         cout << dis[i] << ' ';
+//     }
+
+//     cout << flush;
+//     system("pause");
+//     return 0;
+// }
+
+
+// int main()
+// {
+//     ios::sync_with_stdio(false);
+//     cin.tie(0);
+
+//     int arr[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+//     char str[10] = { 0 };
+//     int cnt = 0;
+//     for (int i = 1; i <= 12; i++)
+//     {
+//         for (int j = 1; j <= arr[i]; j++)
+//         {
+//             sprintf(str, "%02d%02d", i, j);
+//             if (str[0] == str[1] - 1 && str[0] == str[2] - 2) cnt++;
+//             else if (str[1] == str[2] - 1 && str[1] == str[3] - 2) cnt++;
+//         }
+//     }
+//     cout << cnt;
+
+//     cout << flush;
+//     system("pause");
+//     return 0;
+// }
 class edge
 {
-public:
-    int from, to, cost;
-    edge()
-    {
-        
-    }
     
-    edge(const int& a, const int& b, const int& c)
-    {
-        from = a;
-        to = b;
-        cost = c;
-    }
 };
-const int max_v = 1005, max_e = 10005, inf = 0x3f3f3f3f;
-vector<edge> G(max_e);
-int dis[max_v];
-int V, E;
 
-void bellman_ford(int s)
+void spfa(int s)
 {
-    for (int i = 0; i < V; i++) dis[i] = inf;
-    dis[s] = 0;
-    while (1)
-    {
-        bool update = 0;
-        for (int i = 0; i < E; i++)
-        {
-            edge& e = G[i];
-            if (dis[e.from] != inf && dis[e.to] > dis[e.from] + e.cost)
-            {
-                dis[e.to] = dis[e.from] + e.cost;
-                update = 1;
-            }
-        }
-        if (!update) break;
-    }
-}
-
-bool find_negative_loop()
-{
-    for (int i = 0; i < V; i++) dis[i] = 0;
-
-    for (int i = 0; i < V; i++)
-    {
-        for (int j = 0; j < E; j++)
-        {
-            edge& e = G[j];
-            if (dis[e.to] > dis[e.from] + e.cost)
-            {
-                dis[e.to] = dis[e.from] + e.cost;
-                if (i == V - 1) return true;
-            }
-        }
-    }
-
-    return false;
+    
 }
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
+
     
-    cin >> V >> E;
-    for (int i = 0; i < E; i++)
-    {
-        int a, b, c;
-        cin >> a >> b >> c;
-        G[i].from = a;
-        G[i].to = b;
-        G[i].cost = c;
-    }
-    if (find_negative_loop())
-        cout << "yes\n";
-    else
-        cout << "no\n";
-    
-    bellman_ford(0);
-    for (int i = 0; i < V; i++)
-    {
-        cout << dis[i] << ' ';
-    }
 
     cout << flush;
     system("pause");
