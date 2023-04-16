13fe8d0209a56c4639429f46365727c18ed6631c
uuufa <trump@niubi.com>
2023-04-16 21:11:12 +0800

    4.12

---
+++ 2023_April/4_12.cpp
@@ -0,0 +1,18 @@
+// 2023/04/14 13:47:11
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n, m;
+    cin >> n >> m;
+
+    cout << flush;
+    system("pause");
+    return 0;
+}
