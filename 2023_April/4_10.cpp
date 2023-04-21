6fdaffab23119f1d4234cde3dc78260c0a3d4177
uuufa <trump@niubi.com>
2023-04-21 13:45:45 +0800

    4.10

---
+++ 2023_April/4_10.cpp
@@ -0,0 +1,18 @@
+// 2023/04/19 22:57:15
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    string a = "3729C60457BFDAE760723C9BF017E3143CE0E3125B6EFA98AFCE40E603B60A616976BBD9268EAF1B2F462F2E82F3A2CDAF4011B56635079EEA7771ED7DD0F409";
+    cout << a.size();
+
+    cout << flush;
+    system("pause");
+    return 0;
+}
