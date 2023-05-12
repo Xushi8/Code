0005f048c611d04335cbdb1ca668c499e3a531e5
uuufa <trump@niubi.com>
2023-05-12 19:22:41 +0800

    5_12

---
+++ may/5_12.cc
@@ -0,0 +1,16 @@
+// 2023/05/12 15:48:07
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    
+    
+    cout << endl;
+    return 0;
+}
