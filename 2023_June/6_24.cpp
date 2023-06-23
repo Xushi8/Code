0da9e9fe1e24e0b402fd9683c3b2794c61cfdf8c
uuufa <trump@niubi.com>
2023-06-23 16:44:11 +0800

    linux

---
+++ 2023_June/6_24.cpp
@@ -0,0 +1,23 @@
+// 2023/06/23 16:39:26
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+const int INF = 0x3f3f3f3f;
+const ll MOD = (ll)(1e9 + 7);
+const int N = 100005;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    cout << n;
+
+    cout << endl;
+    return 0;
+}
