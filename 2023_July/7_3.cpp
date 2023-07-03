f479e1418c7c8308ae984dfeed4118cbed13080b
uuufa <trump@niubi.com>
2023-07-03 18:47:25 +0800

    .vscode

---
+++ 2023_July/7_3.cpp
@@ -0,0 +1,33 @@
+// 2023/07/03 18:43:29
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdint>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <functional>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+const int INF = 0x3f3f3f3f;
+const int MOD = (int)(1e9 + 7);
+const int N = 100005;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    for (int i = 0; i < 1000; i++)
+        cout << i << '\n';
+
+    cout << endl;
+    return 0;
+}
