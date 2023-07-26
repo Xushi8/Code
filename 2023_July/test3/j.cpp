71477e9dcddd009a4b1cd2d41501962bcb2014a6
uuufa <trump@niubi.com>
2023-07-26 22:24:31 +0800

    test3

--- 2023_July/test3/j.cpp
+++ 2023_July/test3/j.cpp
@@ -20,6 +20,77 @@ using pii = pair<int, int>;
 const int INF = 0x3f3f3f3f;
 const int MOD = (int)(1e9 + 7);
 const int N = 1000005;
+string a, b;
+bool vis[N];
+
+class Node
+{
+public:
+
+    string num;
+    int cnt;
+};
+
+Node n1, n2;
+
+int bfs()
+{
+    queue<Node> que;
+    n1.num = a;
+    n1.cnt = 0;
+    memset(vis, 0, sizeof(vis));
+    vis[stoi(a)] = 1;
+    que.emplace(n1);
+    while (!que.empty())
+    {
+        n1 = que.front(); que.pop();
+        if (n1.num == b)
+            return n1.cnt;
+
+        for (int i = 0; i < 4; i++)
+        {
+            n2 = n1;
+            if (n1.num[i] == '9')
+                n2.num[i] = '1';
+            else
+                n2.num[i] = n1.num[i] + 1;
+            if (!vis[stoi(n2.num)])
+            {
+                n2.cnt = n1.cnt + 1;
+                vis[stoi(n2.num)] = 1;
+                que.emplace(n2);
+            }
+
+            n2 = n1;
+            if (n1.num[i] == '1')
+                n2.num[i] = '9';
+            else
+                n2.num[i] = n1.num[i] - 1;
+            if (!vis[stoi(n2.num)])
+            {
+                n2.cnt = n1.cnt + 1;
+                vis[stoi(n2.num)] = 1;
+                que.emplace(n2);
+            }
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            n2 = n1;
+            n2.num[i] = n1.num[i + 1];
+            n2.num[i + 1] = n1.num[i];
+            if (!vis[stoi(n2.num)])
+            {
+                n2.cnt = n1.cnt + 1;
+                vis[stoi(n2.num)] = 1;
+                que.emplace(n2);
+            }
+        }
+    }
+
+
+    return 0;
+}
 
 int main()
 {
@@ -30,9 +101,8 @@ int main()
     cin >> tt;
     while (tt--)
     {
-        string a, b;
         cin >> a >> b;
-        
+        cout << bfs() << '\n';
     }
 
     cout << endl;
