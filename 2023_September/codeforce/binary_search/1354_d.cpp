ac8719ea64fb0608452ecec9a2365e5681f3271c
uuufa <trump@niubi.com>
2023-10-27 16:02:50 +0800

    10/27 1 1

--- 2023_September/codeforce/binary_search/1354_d.cpp
+++ 2023_September/codeforce/binary_search/1354_d.cpp
@@ -28,8 +28,51 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0);
     
-    int n, q;
-    cin >> n >> q;
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cin >> b[i];
+    }
+    sort(a.begin(), a.end());
+
+    auto check = [&](int val) -> bool
+        {
+            int cnt = a.end() - lower_bound(a.begin(), a.end(), val); // 记录>=val的数目
+            // cerr << cnt << ' ' << lower_bound(a.begin(), a.end(), val) - a.begin() << '\n';
+            int tot = a.size(); // 记录此时的a中的数量
+            for (auto x : b)
+            {
+                if (x > 0)
+                    cnt += (x >= val);
+                else
+                    cnt -= (-x > tot - cnt);
+                
+                if (x > 0)
+                    tot++;
+                else
+                    tot--;
+            }
+
+            return cnt > 0;
+        };
+
+    int l = 0, r = 1e9, ans = 0;
+    while (l <= r)
+    {
+        int mid = l + (r - l) / 2;
+        if (check(mid))
+            ans = mid, l = mid + 1;
+        else
+            r = mid - 1;
+    }
+
+    cout << ans << '\n';
 
 #ifdef LOCAL
     cerr << "Time elapsed: " << clock() / 1000 << " ms" << endl;
