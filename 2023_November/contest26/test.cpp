355f0eda05a7a2a23e7c79415073b93b5d156701
uuufa <trump@niubi.com>
2023-11-25 17:22:12 +0800

    contest26

---
+++ 2023_November/contest26/test.cpp
@@ -0,0 +1,43 @@
+// 2023/11/25 15:34:52
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdint>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <functional>
+#include <iomanip>
+#include <cmath>
+#include <stack>
+#include <array>
+#include <fstream>
+#include <bitset>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+constexpr int N = 1000005;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    for (int i = 0; i < 64; i++)
+    {
+        ull x = 1ULL << i - 1;
+        cout << x << ' ' << ((x - 1) % 7 == 0) << '\n';
+    }
+
+#ifdef LOCAL
+    cerr << "Time elapsed: " << clock() / 1000 << " ms" << endl;
+#endif
+    cout << endl;
+    return 0;
+}
