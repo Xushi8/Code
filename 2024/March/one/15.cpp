4d88811bc789c1f824a819ae182058e5d3e296c4
uuufa <trump@niubi.com>
2024-03-22 14:40:25 +0800

    test1

---
+++ 2024/March/one/15.cpp
@@ -0,0 +1,39 @@
+// 2024/03/22 14:40:21
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdint>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <functional>
+#include <iomanip>
+#include <cmath>
+#include <stack>
+#include <array>
+#include <fstream>
+#include <bitset>
+#include <numeric>
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+constexpr int N = 1000005;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    
+    
+#ifdef LOCAL
+    cerr << "Time elapsed: " << clock() / 1000 << " ms" << endl;
+#endif
+    return 0;
+}
