41861e6f4cd9658433b33d79f605143b17b24543
uuufa <trump@niubi.com>
2024-03-29 18:29:57 +0800

    test

--- 2024/March/three/2-3.cpp
+++ 2024/March/three/2-3.cpp
@@ -1,88 +1,4 @@
-// // 2024/03/27 14:10:12
-// #include <iostream>
-// #include <algorithm>
-// #include <cstring>
-// #include <cstdint>
-// #include <vector>
-// #include <string>
-// #include <set>
-// #include <map>
-// #include <unordered_map>
-// #include <unordered_set>
-// #include <queue>
-// #include <functional>
-// #include <iomanip>
-// #include <cmath>
-// #include <stack>
-// #include <array>
-// #include <fstream>
-// #include <bitset>
-// #include <numeric>
-// using namespace std;
-
-// using ll = long long;
-// using pii = pair<int, int>;
-// using pll = pair<ll, ll>;
-// using psi = pair<string, int>;
-// constexpr int N = 1000005;
-
-// bool cmp(psi const& x, psi const& y)
-// {
-// 	if (x.second != y.second)
-// 		return x.second > y.second;
-// 	else
-// 		return x.first < y.first;
-// }
-
-// int main()
-// {
-// 	ios::sync_with_stdio(false);
-// 	cin.tie(0);
-
-// 	int n, g, k;
-// 	cin >> n >> g >> k;
-// 	int sum = 0;
-// 	vector<psi> a(n);
-// 	for (int i = 0; i < n; i++)
-// 	{
-// 		string x;
-// 		int y;
-// 		cin >> x >> y;
-// 		a[i] = {x, y};
-// 		if (y >= g)
-// 			sum += 50;
-// 		else if (y >= 60)
-// 			sum += 20;
-// 	}
-
-// 	sort(a.begin(), a.end(), cmp);
-// 	cout << sum << '\n';
-// 	vector<int> rk(n);
-// 	rk[0] = 1;
-// 	for (int i = 1; i < n; i++)
-// 	{
-// 		if (a[i].second == a[i - 1].second)
-// 			rk[i] = rk[i - 1];
-// 		else
-// 			rk[i] = i + 1;
-// 	}
-	
-// 	for (int i = 0; i < n; i++)
-// 	{
-// 		auto [x, y] = a[i];
-// 		int rank = rk[i];
-// 		if (rank > k)
-// 			break;
-// 		cout << rank << ' ' << x << ' ' << y << '\n';
-// 	}
-
-// #ifdef LOCAL
-// 	cerr << "Time elapsed: " << clock() / 1000 << " ms" << endl;
-// #endif
-// 	return 0;
-// }
-
-// 2024/03/27 18:45:22
+// 2024/03/27 14:10:12
 #include <iostream>
 #include <algorithm>
 #include <cstring>
@@ -107,15 +23,59 @@ using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
+using psi = pair<string, int>;
 constexpr int N = 1000005;
+
+bool cmp(psi const& x, psi const& y)
+{
+	if (x.second != y.second)
+		return x.second > y.second;
+	else
+		return x.first < y.first;
+}
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
+
+	int n, g, k;
+	cin >> n >> g >> k;
+	int sum = 0;
+	vector<psi> a(n);
+	for (int i = 0; i < n; i++)
+	{
+		string x;
+		int y;
+		cin >> x >> y;
+		a[i] = {x, y};
+		if (y >= g)
+			sum += 50;
+		else if (y >= 60)
+			sum += 20;
+	}
+
+	sort(a.begin(), a.end(), cmp);
+	cout << sum << '\n';
+	vector<int> rk(n);
+	rk[0] = 1;
+	for (int i = 1; i < n; i++)
+	{
+		if (a[i].second == a[i - 1].second)
+			rk[i] = rk[i - 1];
+		else
+			rk[i] = i + 1;
+	}
 	
-	
-	
+	for (int i = 0; i < n; i++)
+	{
+		auto [x, y] = a[i];
+		int rank = rk[i];
+		if (rank > k)
+			break;
+		cout << rank << ' ' << x << ' ' << y << '\n';
+	}
+
 #ifdef LOCAL
 	cerr << "Time elapsed: " << clock() / 1000 << " ms" << endl;
 #endif
--- 2024/March/three/2-4.cpp
+++ 2024/March/three/2-4.cpp
@@ -157,7 +157,7 @@
 // 	return 0;
 // }
 
-// 2024/03/27 20:35:23
+// 2024/03/29 08:42:29
 #include <iostream>
 #include <algorithm>
 #include <cstring>
@@ -188,9 +188,112 @@ int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	
-	
-	
+
+	int n, m;
+	cin >> n >> m;
+	vector<vector<pair<bool, int>>> G(m);
+	for (int i = 0; i < m; i++)
+	{
+		int k;
+		cin >> k;
+		for (int j = 0; j < k; j++)
+		{
+			string s;
+			cin >> s;
+			bool f = s.front() != '-';
+			if (f)
+				G[i].emplace_back(f, stoi(s) + 1);
+			else
+				G[i].emplace_back(f, -(stoi(s) - 1));
+		}
+
+		sort(G[i].begin(), G[i].end());
+	}
+
+	string s;
+	cin >> s;
+	bool flag = s.front() != '-';
+	pair<bool, int> u, v;
+	if (flag)
+		u = {flag, stoi(s) + 1};
+	else
+		u = {flag, -(stoi(s) - 1)};
+	cin >> s;
+	flag = s.front() != '-';
+	if (flag)
+		v = {flag, stoi(s) + 1};
+	else
+		v = {flag, -(stoi(s) - 1)};
+
+	vector<double> va(n + 1), vb(n + 1);
+
+	for (int i = 0; i < m; i++)
+	{
+		auto it1 = lower_bound(G[i].begin(), G[i].end(), u);
+		auto it2 = lower_bound(G[i].begin(), G[i].end(), v);
+		if (*it1 == u)
+		{
+			for (auto [f, val] : G[i])
+			{
+				if (f == !u.first)
+				{
+					va[val] += 1.0 / G[i].size();
+				}
+			}
+		}
+		if (*it2 == v)
+		{
+			for (auto [f, val] : G[i])
+			{
+				if (f == !v.first)
+				{
+					vb[val] += 1.0 / G[i].size();
+				}
+			}
+		}
+	}
+
+	double max1 = *max_element(va.begin(), va.end());
+	double max2 = *max_element(vb.begin(), vb.end());
+	double eps = 1e-6;
+	if (abs(va[v.second] - max1) < eps && abs(vb[u.second] - max2) < eps)
+	{
+		if (!u.first)
+			cout << '-';
+		cout << u.second - 1 << ' ';
+		if (!v.first)
+			cout << '-';
+		cout << v.second - 1 << '\n';
+	}
+	else
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			if (abs(va[i] - max1) < eps)
+			{
+				if (!u.first)
+					cout << '-';
+				cout << u.second - 1 << ' ';
+				if (u.first)
+					cout << '-';
+				cout << i - 1 << '\n';
+			}
+		}
+
+		for (int i = 1; i <= n; i++)
+		{
+			if (abs(vb[i] - max2) < eps)
+			{
+				if (!v.first)
+					cout << '-';
+				cout << v.second - 1 << ' ';
+				if (v.first)
+					cout << '-';
+				cout << i - 1 << '\n';
+			}
+		}
+	}
+
 #ifdef LOCAL
 	cerr << "Time elapsed: " << clock() / 1000 << " ms" << endl;
 #endif
