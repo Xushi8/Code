c4b32c339c6874ec669895933464f12933ab623b
uuufa <trump@niubi.com>
2024-04-06 17:21:13 +0800

    six

--- 2024/March/six/3-1.cpp
+++ 2024/March/six/3-1.cpp
@@ -21,20 +21,21 @@
 using namespace std;
 
 using ll = long long;
-using pii = pair<int, int>;
+using pii = pair<ll, int>;
 using pll = pair<ll, ll>;
 constexpr int N = 1000005;
 
 struct A
 {
-	int to, w1, w2;
-	A(int to_, int w1_, int w2_) :
+	ll to;
+	int w1, w2;
+	A(ll to_, int w1_, int w2_) :
 		to(to_), w1(w1_), w2(w2_) {}
 };
 
-constexpr int INF = 0x3f3f3f3f;
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
 
-void dijkstra1(int s, vector<int>& dis, vector<vector<A>> const& G)
+void dijkstra1(int s, vector<ll>& dis, vector<vector<A>> const& G)
 {
 	fill(dis.begin(), dis.end(), INF);
 	dis[s] = 0;
@@ -56,9 +57,9 @@ void dijkstra1(int s, vector<int>& dis, vector<vector<A>> const& G)
 	}
 }
 
-void dijkstra2(int s, vector<int>& dis, vector<vector<A>> const& G)
+void dijkstra2(int s, vector<ll>& dis, vector<vector<A>> const& G)
 {
-	fill(dis.begin(), dis.end(), 0x3f3f3f3f);
+	fill(dis.begin(), dis.end(), INF);
 	dis[s] = 0;
 	priority_queue<pii, vector<pii>, greater<>> que;
 	que.emplace(0, s);
@@ -85,7 +86,7 @@ int main()
 
 	int n, m, q;
 	cin >> n >> m >> q;
-	vector<vector<A>> G(n);
+	vector<vector<A>> G1(n), G2(n);
 	for (int i = 0; i < m; i++)
 	{
 		int u, v, w1, w2;
@@ -94,31 +95,41 @@ int main()
 			continue;
 		u--;
 		v--;
-		G[u].emplace_back(v, w1, w2);
+		G1[u].emplace_back(v, w1, w2);
+		G2[v].emplace_back(u, w1, w2);
 	}
 
-	vector<int> dis1(n), dis2(n);
-	dijkstra1(0, dis1, G);
-	dijkstra2(0, dis2, G);
+	vector<ll> dis1(n), dis2(n);
+	dijkstra1(0, dis1, G1);
+	dijkstra2(n - 1, dis2, G2);
 
-	vector<int> val(n);
+	vector<ll> val(n);
 	for (int i = 0; i < n; i++)
 	{
 		cin >> val[i];
 	}
 
-	int pre, minn;
-	for (int i = 0; i < n - 1; i++)
+	multiset<ll> st;
+	for (int i = 0; i < n; i++)
 	{
-		
-    }
+		if (dis1[i] != INF && dis2[i] != INF)
+		{
+			st.emplace(dis1[i] + (dis2[i] + val[i] - 1) / val[i]);
+		}
+	}
 
 	while (q--)
 	{
 		int x, y;
 		cin >> x >> y;
 		x--;
-		val[x] = y;
+		if (dis1[x] != INF && dis2[x] != INF)
+		{
+			st.erase(st.find(dis1[x] + (dis2[x] + val[x] - 1) / val[x]));
+			val[x] = y;
+			st.emplace(dis1[x] + (dis2[x] + val[x] - 1) / val[x]);
+		}
+		cout << *st.begin() << '\n';
 	}
 
 #ifdef LOCAL
