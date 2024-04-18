4bc873163d8e6e230d1a03384a385a145e7320db
uuufa <trump@niubi.com>
2024-04-18 18:40:10 +0800

    consumer_producer

--- 2024/March/consumer_producer.cpp
+++ 2024/March/consumer_producer.cpp
@@ -1,4 +1,5 @@
-#include <fmt/core.h>
+#include <iostream>
+#include <format>
 #include <thread>
 #include <mutex>
 #include <queue>
@@ -9,22 +10,21 @@
 
 namespace thread_test
 {
-using fmt::print;
-using fmt::format;
+using std::cout;
+using std::format;
 using std::jthread;
 using std::mutex;
 using std::queue;
 using std::condition_variable;
-using std::scoped_lock;
 using std::unique_lock;
 using std::vector;
 
 constexpr size_t max_size = 100;
 constexpr size_t produce_num = 100000;
-size_t thread_num;
+size_t produce_thread_num, consume_thread_num;
 std::atomic<size_t> over_thread_num = 0;
 queue<size_t> container;
-bool over = false;
+bool produce_over = false;
 condition_variable cv_producer, cv_consumer;
 mutex mtx;
 
@@ -43,17 +43,17 @@ void producer(size_t id)
 			container_size = container.size();
 		}
 		cv_consumer.notify_one();
-		print("Thread {:<3} push {:<6}; Container size: {:<3}\n", id, i, container_size);
+		cout << format("Thread {:<3} push {:<6}; Container size: {:<3}\n", id, i, container_size);
 		if (container_size > max_size)
 		{
 			throw std::runtime_error("Container overflow!!!");
 		}
 	}
 
-	if (++over_thread_num == thread_num)
+	if (++over_thread_num == produce_thread_num)
 	{
-		print("Produce over.\n");
-		over = true;
+		cout << format("Produce produce_over.\n");
+		produce_over = true;
 		cv_consumer.notify_all();
 	}
 }
@@ -68,7 +68,7 @@ void consumer(size_t id)
 			unique_lock lock(mtx);
 			cv_consumer.wait(lock, [&]()
 				{
-					return !container.empty() || over;
+					return !container.empty() || produce_over;
 				});
 			if (container.empty())
 				break;
@@ -77,7 +77,7 @@ void consumer(size_t id)
 			container_size = container.size();
 		}
 		cv_producer.notify_one();
-		print("Thread {:<3} get  {:<6}; Container size: {:<3}\n", id, gift, container_size);
+		cout << format("Thread {:<3} get  {:<6}; Container size: {:<3}\n", id, gift, container_size);
 		if (container_size > max_size)
 		{
 			throw std::runtime_error("Container overflow!!!");
@@ -87,14 +87,20 @@ void consumer(size_t id)
 
 void test()
 {
-	thread_num = std::thread::hardware_concurrency();
+	size_t all_thread_num = std::thread::hardware_concurrency();
 	vector<jthread> producers, consumers;
-	for (size_t i = 0; i < thread_num; i++)
+	produce_thread_num = all_thread_num / 2;
+	consume_thread_num = all_thread_num - produce_thread_num;
+	for (size_t i = 0; i < produce_thread_num; i++)
 	{
 		producers.emplace_back(producer, i);
+	}
+	for (size_t i = produce_thread_num; i < all_thread_num; i++)
+	{
 		consumers.emplace_back(consumer, i);
 	}
 }
+
 } // namespace thread_test
 
 int main()
