167a5ae1effbf57244c0b18bfc6dc42266e4c61b
uuufa <trump@niubi.com>
2024-05-11 16:20:52 +0800

    os over

---
+++ 2024/May/bool.cpp
@@ -0,0 +1,43 @@
+// 2024/05/11 16:00:09
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdint>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <functional>
+#include <iomanip>
+#include <cmath>
+#include <stack>
+#include <array>
+#include <fstream>
+#include <bitset>
+#include <numeric>
+using namespace std;
+
+using i64 = int64_t;
+using pii = pair<int, int>;
+constexpr int N = 1000005;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+	bool a, b, c;
+	cin >> a >> b >> c;
+	if (cin.good())
+	{
+		cout <<  c << endl;
+    }
+    
+#ifdef LOCAL
+    cerr << "Time elapsed: " << clock() / 1000 << " ms" << endl;
+#endif
+    return 0;
+}
