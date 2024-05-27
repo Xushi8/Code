e998e06305eb87f01f95cc029e7f5762a31c2b07
uuufa <trump@niubi.com>
2024-05-27 12:27:51 +0800

    output

--- 2024/May/output.cpp
+++ 2024/May/output.cpp
@@ -1,14 +1,19 @@
 #include <fstream>
 #include <cstddef>
 #include <vector>
+#include <cstdint>
 using namespace std;
 
 int main()
 {
-	ofstream ofs("test.txt");
+	ofstream ofs("/media/tom/Data/test.txt");
 	constexpr size_t n = 1000000000;
 	vector<size_t> a(n, 0x3f3f3f3f3f3f3f3f);
+	// for (int i = 0; i < 10; i++)
+	// {
 	ofs.write(reinterpret_cast<const char*>(a.data()), a.size() * sizeof(size_t));
-    
+	// }
+
+
 	return 0;
 }
