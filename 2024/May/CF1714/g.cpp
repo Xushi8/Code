52148aa394eb33bf5fd853cfa3ef465b60e86b1f
uuufa <trump@niubi.com>
2024-05-30 21:00:16 +0800

    使用 function 实现递归lambda

--- 2024/May/CF1714/g.cpp
+++ 2024/May/CF1714/g.cpp
@@ -1,4 +1,4 @@
-// 2024/05/30 12:54:46
+// 2024/05/30 20:20:19
 #ifdef LOCAL
 #include <basic_std_lib.h>
 #else
@@ -11,6 +11,47 @@ using pii = pair<int, int>;
 using pll = pair<i64, i64>;
 constexpr int N = 1000005;
 
+void dfs(int u, i64 now_sum, vector<i64>& prefix, map<int, int>& ans, vector<vector<array<int, 3>>> const& G)
+{
+	if (u != 0)
+	{
+		auto it = upper_bound(prefix.begin(), prefix.end(), now_sum);
+		ans[u] = it - prefix.begin() - 1;
+	}
+
+	for (auto [v, w0, w1] : G[u])
+	{
+		prefix.emplace_back(w1 + prefix.back());
+		dfs(v, now_sum + w0, prefix, ans, G);
+		prefix.pop_back();
+	}
+}
+
+// void solve()
+// {
+// 	int n;
+// 	cin >> n;
+// 	vector<vector<array<int, 3>>> G(n);
+// 	for (int i = 1; i < n; i++)
+// 	{
+// 		int par, w0, w1;
+// 		cin >> par >> w0 >> w1;
+// 		par--;
+// 		G[par].push_back({i, w0, w1});
+// 	}
+
+// 	vector<i64> prefix;
+// 	prefix.emplace_back(0);
+// 	map<int, int> ans;
+// 	dfs(0, 0, prefix, ans, G);
+// 	for (auto [x, y] : ans)
+// 	{
+// 		y = max(y, 0);
+// 		cout << y << ' ';
+// 	}
+// 	cout << '\n';
+// }
+
 void solve()
 {
 	int n;
@@ -18,29 +59,68 @@ void solve()
 	vector<vector<array<int, 3>>> G(n);
 	for (int i = 1; i < n; i++)
 	{
-		int par, w1, w2;
-		cin >> par >> w1 >> w2;
+		int par, w0, w1;
+		cin >> par >> w0 >> w1;
 		par--;
-		G[par].push_back({i, w1, w2});
+		G[par].push_back({i, w0, w1});
 	}
 
-	
+	vector<i64> prefix{0};
+	prefix.reserve(n + 1);
+	vector<int> ans(n);
+
+	function<void(int u, i64 now_sum)> dfs = [&](int u, i64 now_sum)
+	{
+		// if (u != 0)
+		{
+			auto it = upper_bound(prefix.begin(), prefix.end(), now_sum);
+			ans[u] = it - prefix.begin() - 1;
+		}
+
+		for (auto [v, w0, w1] : G[u])
+		{
+			prefix.emplace_back(prefix.back() + w1);
+			dfs(v, now_sum + w0);
+			prefix.pop_back();
+		}
+	};
+
+	// auto dfs = [&](int u, i64 now_sum)
+	// {
+	// 	auto it = upper_bound(prefix.begin(), prefix.end(), now_sum);
+	// 	ans[u] = it - prefix.begin() - 1;
+
+	// 	for (auto [v, w0, w1] : G[u])
+	// 	{
+	// 		prefix.emplace_back(prefix.back() + w1);
+	// 		dfs(), v, now_sum + w0);
+	// 		prefix.pop_back();
+	// 	}
+	// }
+
+	dfs(0, 0);
+
+	for (int i = 1; i < n; i++)
+	{
+		cout << ans[i] << ' ';
+	}
+	cout << '\n';
 }
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 
 	int tt;
 	cin >> tt;
 	while (tt--)
 	{
 		solve();
-    }
+	}
 
 #ifdef LOCAL
-    cerr << "Time elapsed: " << clock() / 1000 << " ms" << endl;
+	cerr << "Time elapsed: " << clock() / 1000 << " ms" << endl;
 #endif
-    return 0;
+	return 0;
 }
