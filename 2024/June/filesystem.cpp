1a5b4834a3644630517782529021d70094fb99ce
uuufa <trump@niubi.com>
2024-06-06 22:56:23 +0800

    parallel

--- 2024/June/filesystem.cpp
+++ 2024/June/filesystem.cpp
@@ -3,6 +3,8 @@
 #include <filesystem>
 #include <execution>
 #include <numeric>
+#include <tbb/parallel_for.h>
+#include <tbb/parallel_reduce.h>
 using namespace std;
 using fmt::print;
 namespace fs = std::filesystem;
@@ -61,10 +63,28 @@ int main()
 	// 		siz += fs::is_regular_file(dir_entry) ? fs::file_size(dir_entry) : 0;
 	// 	});
 
-	siz = std::transform_reduce(std::execution::par_unseq, begin(paths), end(paths), siz, std::plus<>(), [&](auto const& dir_entry) -> size_t
+	// siz = std::transform_reduce(std::execution::par_unseq, begin(paths), end(paths), siz, std::plus<>(), [&](auto const& dir_entry) -> size_t
+	// 	{
+	// 		return fs::is_regular_file(dir_entry) ? fs::file_size(dir_entry) : 0;
+	// 	});
+
+	// tbb::parallel_for(tbb::blocked_range<size_t>(0, paths.size()), [&](tbb::blocked_range<size_t> const& r)
+	// 	{
+	// 		for (size_t i = r.begin(); i != r.end(); i++)
+	// 		{
+	// 			siz += fs::is_regular_file(paths[i]) ? fs::file_size(paths[i]) : 0;
+	// 		}
+	// });
+
+	siz = tbb::parallel_reduce(tbb::blocked_range<size_t>(0, paths.size()), siz, [&](tbb::blocked_range<size_t> const& r, size_t local_res)
 		{
-			return fs::is_regular_file(dir_entry) ? fs::file_size(dir_entry) : 0;
-		});
+			for (size_t i = r.begin(); i != r.end(); i++)
+			{
+				// print("{}\n", paths[i].string());
+				local_res += fs::is_regular_file(paths[i]) ? fs::file_size(paths[i]) : 0;
+			}
+			return local_res;
+	}, std::plus<>());
 
 	print("All size: {} bytes\n", siz);
 	print("All size: {} kb\n", siz / 1024.L);
